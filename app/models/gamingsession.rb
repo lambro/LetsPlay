a3def2f46022ee5e5f886e404094d5ee67a732c4
Lamb <lauraambrose@gmail.com>
2014-12-08 16:13:21 +0000

    fixed typo in gamingsessions.rb

--- app/models/gamingsession.rb
+++ app/models/gamingsession.rb
@@ -1,7 +1,7 @@
 class Gamingsession < ActiveRecord::Base
   attr_accessible :active, :console_id1, :console_id2, :console_id3, :created_by, :description, :gsdate, :location_name, :max_age_limit, :min_age_limit, :online, :postcode, :title
 
-    has_many :users, through :user_sessions
+    has_many :users, through: :user_sessions
     has_many :user_sessions
     
 end
