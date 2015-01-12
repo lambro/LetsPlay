520d55f9f3c771012a86f1b677c40b69829152d5
Lamb <lauraambrose@gmail.com>
2015-01-12 14:55:39 +0000

    last update broke the page so reverting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,8 @@
 class SessionsController < Devise::SessionsController
 
     def new
-        self.resource = resource_class.new(sign_in_params)
-        clean_up_passwords(resource)
-        yield resource if block_given?
-        respond_with resource, location: "/letsplay/home"
-      end
+      
+    end
 
     def create
       self.resource = warden.authenticate!(auth_options)
